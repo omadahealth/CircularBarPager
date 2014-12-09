b023b590915723a631ee40b8ccb3cfd264193639
Stoyan Dimitrov <stoyan.dimitrov@omadahealth.com>
2014-12-09 14:57:23 -0800

    init

---
+++ app/src/main/java/stoyan/olivier/circularbarpager/MainActivity.java
@@ -0,0 +1,16 @@
+package stoyan.olivier.circularbarpager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by stoyan on 12/9/14.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
